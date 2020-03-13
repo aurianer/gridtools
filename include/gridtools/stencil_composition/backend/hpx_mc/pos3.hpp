23831e6c66fb922f30f217c030254e32889f534c
aurianer <aurianer@cscs.ch>
2020-03-13 17:58:03 +0100

    Copy mc backend in hpx_mc

---
+++ include/gridtools/stencil_composition/backend/hpx_mc/pos3.hpp
@@ -0,0 +1,24 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2019, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#pragma once
+
+namespace gridtools {
+    namespace hpx_mc {
+        template <class T>
+        struct pos3 {
+            T i, j, k;
+        };
+
+        template <class T>
+        constexpr pos3<T> make_pos3(T i, T j, T k) {
+            return {i, j, k};
+        }
+    } // namespace hpx_mc
+} // namespace gridtools
